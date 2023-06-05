6d7635c4ce63303840a46c3c3831de61a8d7cd0c
alessandro <alessandrodamata6@gmail.com>
2023-06-05 19:30:22 -0300

    back-end dia 2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,32 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { StateModule } from './state/state.module';
+import { CityModule } from './city/city.module';
+import { AddressModule } from './address/address.module';
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development.local'],
+    }),
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      database: process.env.DB_DATABASE,
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      // url: process.env.DB_URL,
+      entities: [`${__dirname}/**/*.entity{.ts,.js}`],
+      migrations: [`${__dirname}/migration/{.ts,*.js}`],
+      migrationsRun: true
+    }),
+    UserModule,
+    StateModule,
+    CityModule,
+    AddressModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller,Get,Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/createUser.dto';
+import { UserService } from './user.service';
+import { UserEntity } from './entities/user.entity';
 
 @Controller('user')
 export class UserController {
+
+    constructor(private readonly userService: UserService){}
+
+    @Post()
+    async createUser( @Body() createUser: CreateUserDto): Promise<UserEntity>{
+        return this.userService.createUser(createUser)
+    }
+
     @Get()
-    async getUsers(){
-        return JSON.stringify({teste: "ABCDEF"})
+    async getAllUser(): Promise<UserEntity[]>{
+        return this.userService.getAllUser()
     }
 }
